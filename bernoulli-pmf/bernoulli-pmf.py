846805e872ba6647ec7297cf3235e3c9c4b90b14
Vaishnavi_Saranga <141993937+VAISHNAVISARANGA@users.noreply.github.com>
2026-03-28 00:26:33 +0530

    Update solution for bernoulli-pmf

--- bernoulli-pmf/bernoulli-pmf.py
+++ bernoulli-pmf/bernoulli-pmf.py
@@ -6,6 +6,8 @@ def bernoulli_pmf_and_moments(x, p):
     """
     # Write code here
     x=np.array(x)
+    if not np.all((x==0) | (x==1)):
+        return None
     pmf= p**x*(1-p)**(1-x)    
     mu=p
     var=p*(1-p)
