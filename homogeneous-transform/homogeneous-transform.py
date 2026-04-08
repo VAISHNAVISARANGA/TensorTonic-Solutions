54a173465f9cd8a8bffaf6926c58b39b7225b024
Vaishnavi_Saranga <141993937+VAISHNAVISARANGA@users.noreply.github.com>
2026-04-08 21:47:43 +0530

    Add solution for homogeneous-transform

---
+++ homogeneous-transform/homogeneous-transform.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def apply_homogeneous_transform(T, points):
+    """
+    Apply 4x4 homogeneous transform T to 3D point(s).
+    """
+    # Your code here\
+    points=np.asarray(points)
+    if points.ndim==1:
+        p_h=np.append(points, 1)
+        transformed= T@p_h
+        return transformed[ :3]
+    else:
+        ones=np.ones((points.shape[0],1))
+        p_h=np.hstack((points, ones))
+        transformed= (T @p_h.T).T
+        return transformed[:,:3]
+        
+        
