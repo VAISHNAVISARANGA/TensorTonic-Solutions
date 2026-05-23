f98e6619ad3ed0767dc98e6e739b6dc5362e0a38
Vaishnavi_Saranga <141993937+VAISHNAVISARANGA@users.noreply.github.com>
2026-05-23 11:07:23 +0530

    Update solution for gini-impurity

--- gini-impurity/gini-impurity.py
+++ gini-impurity/gini-impurity.py
@@ -1,28 +1,22 @@
 import numpy as np
-
+def _gini(p):
+    label, count= np.unique(p, return_counts=True)
+    p=count/np.sum(count)
+    return 1-np.sum(p**2)
 def gini_impurity(y_left, y_right):
     """
     Compute weighted Gini impurity for a binary split.
     """
     # Write code here
-    y_left=np.asarray(y_left)
-    y_right= np.asarray(y_right)
-
-    value_left, count_left= np.unique(y_left, return_counts=True)
-    value_right, count_right= np.unique(y_right, return_counts=True)
-    p_left=count_left/len(y_left)
-    p_left=p_left[p_left>0]
-    p_right= count_right/len(y_right)
-    p_right=p_right[p_right>0]
-
-    gini_left=1-np.sum(p_left**2)
-    gini_right=1-np.sum(p_right**2)
-
-    N_l= len(y_left)
-    N_r=len(y_right)
-    N=N_l+N_r
-    if (N>0):
-        gini=(N_l/N)*gini_left + (N_r/N)*gini_right
+    y_left= np.array(y_left)
+    y_right=np.array(y_right)
+    N_L=len(y_left)
+    N_R=len(y_right)
+    N=N_L+N_R
+    if(N!=0):
+        gini_split= (N_L/N)*(_gini(y_left))+(N_R/N)*(_gini(y_right))
     else:
-        gini=0
-    return float(gini)
+        gini_split=0
+    return float(gini_split)
+    
+    pass
