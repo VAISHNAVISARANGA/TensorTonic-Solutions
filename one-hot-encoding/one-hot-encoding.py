637170d8ec459bc9f963d27a27e2b78bd59c948e
Vaishnavi_Saranga <141993937+VAISHNAVISARANGA@users.noreply.github.com>
2026-06-09 13:33:38 +0530

    Solve one-hot-encoding

---
+++ one-hot-encoding/one-hot-encoding.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def one_hot(y, num_classes=None):
+    """
+    Convert integer labels y ∈ {0,...,K-1} into one-hot matrix of shape (N, K).
+    """
+    # Write code here
+    y=np.array(y)
+    if num_classes==None:
+        num_classes=np.max(y)+1
+    X=np.zeros((len(y), num_classes))
+    X[np.arange(len(y)),y]=1
+    
+    return X
